479d41136010899153b5c5a67baa99dce78a3944
Masoumi-shima <masoumi.shimaa@gmail.com>
2022-08-26 13:05:06 -0400

    Add transition feature

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
 <TheNavigation />
   <div class="container">
-    <router-view />
+    <router-view v-slot="{Component}">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" :key="$route.path"></component>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -15,3 +19,16 @@ export default {
   }
 }
 </script>
+
+<style lang="css">
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 0.3s;
+  }
+
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+</style>
