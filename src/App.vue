5535ed60c1f5ee45cc761bd61d867624029e1d05
Masoumi-shima <masoumi.shimaa@gmail.com>
2022-08-26 15:51:47 -0400

    Add a left side bar to protected pages

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
+
 <TheNavigation />
+
   <div class="container">
-    <router-view v-slot="{Component}">
-      <transition name="fade" mode="out-in">
-        <component :is="Component" :key="$route.path"></component>
-      </transition>
+
+    <router-view v-slot="{Component}" class="view lef-sidebar" name="LeftSideBar">
+
+        <transition name="fade" mode="out-in">
+
+          <component :is="Component" :key="$route.path"></component>
+
+        </transition>
+
     </router-view>
+
+    <router-view v-slot="{Component}" class="main-view">
+
+      <transition name="fade" mode="out-in">
+
+        <component :is="Component" :key="$route.path"></component>
+
+      </transition>
+
+    </router-view>
+
   </div>
+
 </template>
 
 <script>
@@ -29,6 +48,18 @@ export default {
   .fade-enter,
   .fade-leave-to {
     opacity: 0;
+  }
+
+  .container {
+    display: flex;
+  }
+
+  .left-sidebar {
+    width: 20%;
+  }
+
+  .main-view {
+    width: 100%;
   }
 
 </style>
